346dbe397b1b9b791c1c5e36cfe2de761c8a2335
facorda <acorda.fred@gmail.com>
2021-06-17 21:17:04 -0400

    2.3.3 Storybook initialized.

---
+++ .storybook/preview.js
@@ -0,0 +1,25 @@
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// }
+import { GlobalStyle } from "../src/utils";
+
+export const decorators = [
+  (Story, context) => (
+    <>
+      <GlobalStyle />
+      <main>
+        <Story {...context} />
+      </main>
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
